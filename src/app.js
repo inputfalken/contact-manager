0c7323ca13b798929dba53ec1e1e05d19d7177d7
Robert Andersson <inputfalken@gmail.com>
2017-04-12 14:10:33 +0200

    Fix spell mistake

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ export class App {
       },
       {
         route: 'contacts/:id',
-        moduleId: 'concats-details',
+        moduleId: 'contact-detail',
         name: 'contacts'
       }
     ]);
