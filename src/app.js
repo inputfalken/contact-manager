daf663a3ab469d8efc40da365fbb3671b0c84697
Robert Andersson <inputfalken@gmail.com>
2017-04-12 14:43:03 +0200

    Add loading indicator to view

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
+import {WebAPI} from './web-api';
+
 export class App {
+  static inject() { return [WebAPI]; }
+
+  constructor(api) {
+    this.api = api;
+  }
+
   configureRouter(config, router) {
     config.title = 'Contacts';
     config.map([
-      {
-        route: '',
-        moduleId: 'no-selection',
-        title: 'select'
-      },
-      {
-        route: 'contacts/:id',
-        moduleId: 'contact-detail',
-        name: 'contacts'
-      }
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name: 'contacts' }
     ]);
+
     this.router = router;
   }
 }
-
