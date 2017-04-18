ccabd3da5286577b94bf5b92caa019046a2a973b
Robert Andersson <inputfalken@gmail.com>
2017-04-18 15:52:39 +0200

    Connect with WebAPI

--- src/add-contact.js
+++ src/add-contact.js
@@ -1,3 +1,18 @@
-export class AddContact {
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {ContactAdded} from './message';
+import {WebAPI} from './web-api';
 
+export class AddContact {
+  static inject() { return [WebAPI, EventAggregator]; }
+
+  constructor(api, eventAggregator) {
+    this.api = api;
+    this.eventAggregator = eventAggregator;
+  }
+
+  save() {
+    this.api.addContact(this.contact).then(contact => {
+      this.eventAggregator.publish(new ContactAdded(contact));
+    });
+  }
 }
